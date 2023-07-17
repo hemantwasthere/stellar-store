4da1fde82d67b7f3e7b1b5ac418d78f318de035a
Hemant <hrsmart2150@gmail.com>
2023-07-17 20:18:46 +0530

    Added footer and navbar

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import React from 'react'
+
+const HomePage: NextPage = () => {
+    return (
+        <div className="">
+            HomePage
+        </div>
+    )
+}
+
+export default HomePage
