d6fb4c2f07b63d9aca40e8ae04c9e1e9a11633a0
Hemant <hrsmart2150@gmail.com>
2023-07-19 16:17:20 +0530

    removed unused import

--- app/(routes)/cart/components/cart-item.tsx
+++ app/(routes)/cart/components/cart-item.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
-import { toast } from "react-hot-toast";
 import { X } from "lucide-react";
 
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
 import useCart from "@/hooks/use-cart";
 import { Product } from "@/types";
-
 
 interface CartItemProps {
     data: Product;
