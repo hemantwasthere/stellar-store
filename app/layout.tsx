216869ce8342892669c0fef6d9313c2e2ce5504a
Hemant <hrsmart2150@gmail.com>
2023-07-19 15:27:41 +0530

    Added product preview functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Urbanist } from 'next/font/google'
 
 import Footer from '@/components/footer'
 import Navbar from '@/components/navbar'
+import ToastProvider from '@/providers/toast-provider'
+import ModalProvider from '@/providers/modal-provider'
 
 import './globals.css'
 
@@ -17,6 +19,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToastProvider />
+        <ModalProvider />
         <Navbar />
         {children}
         <Footer />
