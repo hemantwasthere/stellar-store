3a6382f1b1d23102fc9e5f9f649363804e5bcb32
Hemant <hrsmart2150@gmail.com>
2023-07-24 18:49:09 +0530

    changed name again

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = async () => {
             <Container>
                 <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
                     <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-                        <p className="font-bold text-xl">ChicHaven</p>
+                        <p className="font-bold text-xl">StellarStore</p>
                     </Link>
                     <MainNav data={categories} />
                     <NavbarActions />
