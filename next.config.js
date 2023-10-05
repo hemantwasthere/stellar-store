e5dd4c015e5d8aaed08c0e157af92faa2c4a20da
Hemant <hrsmart2150@gmail.com>
2023-10-05 22:02:54 +0530

    Added @netlify/plugin-nextjs pkg for netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['res.cloudinary.com']
-    }
-}
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  experimental: { appDir: true },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
