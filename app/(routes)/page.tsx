4a47b999f6debf404b65657bcce47d6b44939ca2
Hemant <hrsmart2150@gmail.com>
2024-04-14 23:52:06 +0530

    Refactored home page to use new billboard ID

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,5 @@
 import { NextPage } from "next";
 
-
 import getBillboard from "@/actions/get-billboard";
 import getProducts from "@/actions/get-products";
 import ProductList from "@/components/product-list";
@@ -10,19 +9,19 @@ import Container from "@/components/ui/container";
 export const revalidate = 0;
 
 const HomePage: NextPage = async () => {
-    const products = await getProducts({ isFeatured: true });
-    const billboard = await getBillboard("clk9v35ar0001lb09hpqa1qwy");
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("cluzu7aye0001jv077mrkvhs9");
 
-    return (
-        <Container>
-            <div className="space-y-10 pb-10">
-                <Billboard data={billboard} />
-                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                    <ProductList title="Featured Products" items={products} />
-                </div>
-            </div>
-        </Container>
-    )
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
